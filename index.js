0d7fd535a9f521898d24b7f58f7a2210ce4ffe62
Ivanildo Pinto Miranda <112912153+MirandaIvanildo@users.noreply.github.com>
2023-05-12 22:54:42 -0300

    Um exercício de js para divertir
    Como é bom programar

---
+++ index.js
@@ -0,0 +1,40 @@
+// Exercicio 1: hora de codar
+//Escreva um programa em Javascript e crie uma variável chamada "nome_do_carro" e atribua-a um valor "Fusca". Exiba-a ao usuário
+
+
+//var nome_do_carro = prompt ("Qual é o nome do carro: ");
+//console.log("O nome do carro é: " + nome_do_carro);
+
+
+
+
+//Exercicio 2: hora de codar
+//Escreva um programa em Javascript em que o usuário informe o seu nome e exiba a mensagem "Olá, [NomeDoUsuario]
+
+//var nomeDoUsuario = prompt("Digite o seu nome:");
+//console.log("Olá, " + nomeDoUsuario + "!");
+
+
+
+
+
+
+//Exercicio 3: hora de codar
+//Escreva um programa em Javascript em que o usuário informe o seu nome e em seguida o
+// programa perguntará a idade do usuário. Agora o programa deve exibir a mensagem "Olá, [NomeDoUsuario], sua idade é [idade
+
+
+//var nomeDoUsuario = prompt("Digite o seu nome:");
+//var idadeDoUsuario = prompt("Digite a sua idade:");
+
+//console.log("Olá, " + nomeDoUsuario + ", sua idade é " + idadeDoUsuario + " anos!");
+
+
+
+
+// Exercico 4: hora de codar
+//var baseRetangulo = prompt("Digite a base do retângulo:");
+//var alturaRetangulo = prompt("Digite a altura do retângulo:");
+
+//var areaRetangulo = baseRetangulo * alturaRetangulo;
+//console.log("A área do retângulo é: " + areaRetangulo);
